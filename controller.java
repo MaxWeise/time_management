aac46b6aacbf5e8b29382bf0ba1e1d98324fce3f
Max <69238451+MrDax0001@users.noreply.github.com>
2020-09-05 22:59:36 +0200

    Add files via upload

--- controller.java
+++ controller.java
@@ -8,8 +8,16 @@ public class controller {
 	// Static reference used as global variables
 	static ArrayList<Task> taskCollection = new ArrayList<Task>();
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
+		//Default tasks | will be deleted as soon as t.name == input error is solved
+		Task x = new Task("x", 'p', 0, false, 0);
+		taskCollection.add(x);
+		Task y = new Task("y", 'p', 0, false, 0);
+		taskCollection.add(y);
+		Task z = new Task("z", 'p', 0, false, 0);
+		taskCollection.add(z);
 		/**
 		 * Three steps:
 		 * 		1. Load saved data
@@ -23,38 +31,70 @@ public class controller {
 
 		// ==========================================
 
-		// TODO: Implement programm here
+		// TODO: Implement program here
 
 		boolean CONTINUE = true;
 		Scanner user = new Scanner(System.in);
 
 		do {
 
-			printList();
-			System.out.println("Please enter command:");
-			String userInput = user.nextLine().toLowerCase();
+			printList(); // print taskCollection
+			System.out.println("Please enter command: ");
+			String userInput = user.next().toLowerCase();
 
 			// Keywords / Methods get selected here
 			switch (userInput) {
 			case "new":
-				// TODO: implement task construction
-				// Scanner userData_name = new Scanner(System.in);
-				// Scanner userData_module = new Scanner(System.in);
-				//
-				// System.out.println("Please enter name of task:");
-				// String taskName = userData_name.nextLine();
-				//// char taskModule = userData_module.nextLine();
+				Scanner S_name = new Scanner(System.in);
+				System.out.println("Please enter name of task:");
+				String n = S_name.nextLine();
 
-				// taskCollection.add(t = new Task(taskName, taskModule, 0,
-				// false, 0));
+				System.out.println("Please enter module of task (p | c | d):");
+				Scanner S_module = new Scanner(System.in);
+				char m = S_module.next().charAt(0);
+
+				System.out.println("Please enter priority (-1 | 0 | 1):");
+				Scanner S_priority = new Scanner(System.in);
+				int p = S_priority.nextInt();
+
+				Task t = new Task(n, m, 0, false, p);
+
+				taskCollection.add(t);
+
 				break;
 
 			case "delete":
 				// TODO: implement task destruction
+				// TODO: name of task and input are not the same, why??
+
+				// This is just a workaround
+				Scanner input_index = new Scanner(System.in);
+				for (int i = 0; i < taskCollection.size(); i++) {
+					System.out.print((i + 1) + ":   ");
+					System.out.println(taskCollection.get(i));
+				}
+
+				System.out.println("Please input number of task you want to delete:");
+				int index = input_index.nextInt();
+
+				taskCollection.remove(index - 1);
+
+				// ==========================================
+
+				// for (Task a : taskCollection) {
+				// if (a.name == delete_element) {
+				// taskCollection.remove(a);
+				// break;
+				// }
+				// }
+				//
+				// System.out.println("Could not find task. Please try again");
+
 				break;
 
 			case "work":
 				// TODO: implement code to work on task
+
 				break;
 
 			case "exit":
