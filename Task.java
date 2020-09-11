f3420b22c81c9c021075181bb01f64cab8ceac4a
Max <69238451+MrDax0001@users.noreply.github.com>
2020-09-11 22:54:55 +0200

    Add files via upload

--- Task.java
+++ Task.java
@@ -2,23 +2,13 @@ package organisationsprogramm_v2_java;
 
 import java.util.Scanner;
 
-// import org.apache.commons.lang.time.StopWatch;
-
 /**
  * @author Max Weise
  * 
- * Created on 04.09.2020
- * Class to model all the given tasks in the programm.
+ *         Created on 04.09.2020 Class to model all the given tasks in the
+ *         program.
  */
-class Task {
-
-	/**
-	 * name     -> descriptive name of the task
-	 * module   -> code, plan, debug
-	 * time     -> time in seconds, total time spent on task
-	 * wip      -> is task in progress?
-	 * priority -> -1, 0, 1; 0 is normal, the higher, the more important
-	 */
+class Task implements I_Time {
 
 	String name;
 	char module;
@@ -26,9 +16,22 @@ class Task {
 	boolean work_in_progress;
 	int priority;
 
+	public Task() {
+		// Empty constrctor
+	}
+
+	public Task(String name, int time, boolean wip, int priority) {
+		this();
+		this.name = this.setName(name);
+		this.module = '-';
+		this.time = time;
+		this.work_in_progress = wip;
+		this.priority = priority;
+	}
+
 	public Task(String name, char module, int time, boolean wip, int priority) {
 		this.name = this.setName(name);
-		this.module = module;
+		this.module = setModule(module);
 		this.time = time;
 		this.work_in_progress = wip;
 		this.priority = priority;
@@ -36,6 +39,7 @@ class Task {
 
 	/**
 	 * Sets name for task. (May not contain spaces, but has yet to be implemented)
+	 * 
 	 * @param nameToChange (Will later be processed for spaces)
 	 * @return nameToChange
 	 */
@@ -43,41 +47,56 @@ class Task {
 		return nameToChange;
 	}
 
-	/**
-	 * Used to measure time in milliseconds, ms gets converted into seconds. 
-	 * @return time passed in seconds
-	 */
-	int stopwatch() {
-		int seconds = 0;
-		long t0 = 0;
-		long t1 = 0;
-		
-		System.out.println("Started stopwatch");
-		
-		t0 = System.currentTimeMillis();
-		
-		System.out.println("Type <end> to stop the stopwatch");
-		Scanner endStopwatch = new Scanner(System.in);
-		String a = endStopwatch.nextLine();
-		
-		if(a == "")
-		
-		endStopwatch.close();
-		t1 = System.currentTimeMillis();
-			
-		seconds = (int) (t1 - t0) / 1000;
-		this.time += seconds;
-		
-		return seconds;
+	char setModule(char moduleToChange) {
+		if (!(moduleToChange == 'p' || moduleToChange == 'c' || moduleToChange == 'd')) {
+			return '-';
+		} else {
+			return moduleToChange;
+		}
 	}
 
 	/**
-	 * overrides build in toString method 
+	 * Used to measure time in milliseconds, ms gets converted into seconds. Time
+	 * gets started automatically, when key is pressed time will stop and measured
+	 * time gets added to total time spent on Task
+	 * 
+	 * @return time passed in seconds
+	 */
+	@SuppressWarnings("resource")
+	public void stopwatch() {
+		int seconds = 0;
+		long t0 = 0;
+		long t1 = 0;
+
+		System.out.println("Started stopwatch");
+
+		t0 = System.currentTimeMillis();
+
+		System.out.println("Press enter to stop the stopwatch");
+		Scanner endStopwatch = new Scanner(System.in);
+		String a = endStopwatch.nextLine();
+
+		if (a == "") { // Stop time when key is pressed
+		}
+
+		t1 = System.currentTimeMillis();
+
+		seconds = (int) (t1 - t0) / 1000;
+		this.time += seconds;
+		System.out
+				.println("Time spent: " + I_Time.timeDelta(seconds) + " | Total time: " + I_Time.timeDelta(this.time));
+		return;
+	}
+
+	/**
+	 * overrides build in toString method
+	 * 
 	 * @return Task in following format: [priority] name (module | time )
 	 */
 	@Override
 	public String toString() {
-		return "[" + this.priority + "] " + this.name + " (" + this.module + "| T: " + this.time + " )";
-
+		String output = String.format("[%d] %s (%c |T: %s)", this.priority, this.name, this.module,
+				I_Time.timeDelta(this.time));
+		return output;
 	}
 } // End of Class
--- controller.java
+++ controller.java
@@ -6,18 +6,20 @@ import java.util.Scanner; // Import for user input
 public class controller {
 
 	// Static reference used as global variables
-	static ArrayList<Task> taskCollection = new ArrayList<Task>();
+	static ArrayList<Task> toDoList = new ArrayList<Task>();
+	static boolean CONTINUE = true;
+	static int totalTime = 0; // in s
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
-		//Default tasks | will be deleted as soon as t.name == input error is solved
-		Task x = new Task("x", 'p', 0, false, 0);
-		taskCollection.add(x);
+		// Default tasks
+		// solved
+		Task x = new Task("x", 0, false, 0);
+		toDoList.add(x);
 		Task y = new Task("y", 'p', 0, false, 0);
-		taskCollection.add(y);
-		Task z = new Task("z", 'p', 0, false, 0);
-		taskCollection.add(z);
+		toDoList.add(y);
+		Task z = new Task("z", 'a', 0, false, 0);
+		toDoList.add(z);
 		/**
 		 * Three steps:
 		 * 		1. Load saved data
@@ -25,22 +27,31 @@ public class controller {
 		 * 		3. Save all relevant data
 		 */
 
-		// Necessary declarations here
-
 		// TODO: Load data here
 
 		// ==========================================
 
 		// TODO: Implement program here
 
-		boolean CONTINUE = true;
-		Scanner user = new Scanner(System.in);
-
 		do {
 
-			printList(); // print taskCollection
-			System.out.println("Please enter command: ");
-			String userInput = user.next().toLowerCase();
+			// Update total time
+			totalTime = 0;
+			for (Task t : toDoList) {
+				totalTime += t.time;
+			}
+
+			@SuppressWarnings("resource")
+			Scanner user = new Scanner(System.in);
+
+			if (toDoList.isEmpty()) {
+				System.out.println("There are no tasks in this List");
+			} else {
+				printToDoList(); // print taskCollection
+			}
+
+			System.out.println("\nPlease enter command: ");
+			String userInput = user.nextLine().toLowerCase();
 
 			// Keywords / Methods get selected here
 			switch (userInput) {
@@ -57,43 +68,32 @@ public class controller {
 				Scanner S_priority = new Scanner(System.in);
 				int p = S_priority.nextInt();
 
-				Task t = new Task(n, m, 0, false, p);
+				Task newTask = new Task(n, m, 0, false, p);
 
-				taskCollection.add(t);
+				toDoList.add(newTask);
 
 				break;
 
 			case "delete":
-				// TODO: implement task destruction
-				// TODO: name of task and input are not the same, why??
-
-				// This is just a workaround
-				Scanner input_index = new Scanner(System.in);
-				for (int i = 0; i < taskCollection.size(); i++) {
-					System.out.print((i + 1) + ":   ");
-					System.out.println(taskCollection.get(i));
+				if (deleteTask()) {
+					System.out.println("Task has been sucessfully removed");
+				} else {
+					System.out.println("Something went wrong!\nPlease try again.");
 				}
-
-				System.out.println("Please input number of task you want to delete:");
-				int index = input_index.nextInt();
-
-				taskCollection.remove(index - 1);
-
-				// ==========================================
-
-				// for (Task a : taskCollection) {
-				// if (a.name == delete_element) {
-				// taskCollection.remove(a);
-				// break;
-				// }
-				// }
-				//
-				// System.out.println("Could not find task. Please try again");
 
 				break;
 
 			case "work":
 				// TODO: implement code to work on task
+				System.out.println("Please enter name of Task:");
+				Scanner input_name = new Scanner(System.in);
+				String element = input_name.next();
+
+				for (Task workTask : toDoList) {
+					if (workTask.name.equalsIgnoreCase(element)) {
+						workTask.stopwatch();
+					}
+				}
 
 				break;
 
@@ -112,14 +112,33 @@ public class controller {
 		// TODO: Save data here
 
 		System.out.println("END");
-		user.close();
 
 	} // End of main
 
-	static void printList() {
-		for (Task a : taskCollection) {
+	/** Auxiliary function to easily print all the Task elements in the Task list */
+	static void printToDoList() {
+		for (Task a : toDoList) {
 			System.out.println(a);
 		}
+
+		System.out.println("\nTotal time " + I_Time.timeDelta(totalTime));
+		System.out.println("=======================");
+	}
+
+	@SuppressWarnings("resource")
+	static boolean deleteTask() {
+		System.out.println("Please enter name of Task to delete:");
+		Scanner input_name = new Scanner(System.in);
+		String delete_element = input_name.next();
+
+		for (Task a : toDoList) {
+			if (a.name.equalsIgnoreCase(delete_element)) {
+				toDoList.remove(a);
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 } // End of Class
